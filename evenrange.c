4d8d02463f74066afbcc3a8a15684d4db2efbd19
6087praveen <35293920+6087praveen@users.noreply.github.com>
2018-01-24 15:53:55 +0530

    Create evenrange.c

---
+++ evenrange.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,p,q;
+	printf("enter the value p and q");
+	scanf("%d%d",&p,&q);
+	printf("print the even number for the given range p to q:\n");
+	for(i=p+1;i<q;i++)
+	{
+		if(i%2==0)
+		printf("%d\n",i);
+	}
+	return 0;
+}
