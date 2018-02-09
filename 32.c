26c9fa84aa729093ba904cb7e3c25fc4bec5145b
6087praveen <35293920+6087praveen@users.noreply.github.com>
2018-02-09 16:09:53 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+   char a[400];
+   int i;
+   int count=0;
+   scanf("%[^\n]s",a);
+   for(i=0;a[i]!='\0';i++)
+   {
+       if(a[i]==' ')
+       {
+    count++;
+       }
+   }
+   printf("%d",++count);
+   return 0;
+}
