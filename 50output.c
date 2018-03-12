c96f7572b780088d9582ba9119b49bcc465cdae1
6087praveen <35293920+6087praveen@users.noreply.github.com>
2018-03-12 16:46:45 +0530

    Create 50output.c

---
+++ 50output.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int b,a;
+    scanf("%d",&b);
+    a=b*b;
+    if(a>=0&&a<=9)
+    {
+    printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
+
+
+
