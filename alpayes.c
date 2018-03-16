73f73d9fe3c5aa86e75749d78fba63abfae0bae2
6087praveen <35293920+6087praveen@users.noreply.github.com>
2018-03-16 16:35:32 +0530

    Create alpayes.c

---
+++ alpayes.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char a[100];
+	int i;
+	scanf("%s",&a[i]);
+	if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'||a[i]<='Z')||(a[i]>='0'&&a[i]<='9'))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
